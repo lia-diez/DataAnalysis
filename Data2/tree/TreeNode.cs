cdb3c49c6ff141ea9430d5f3eaccd8fd7f2b0ce3
Hlushchenko <hluushchenko@gmail.com>
2022-02-15 14:18:58 +0200

    Tree added

---
+++ Data2/tree/TreeNode.cs
@@ -0,0 +1,22 @@
+﻿namespace Data2.tree;
+
+public class TreeNode
+{
+    public TreeNode(string? value = null, TreeNode? parent = null)
+    {
+        Value = value;
+        Parent = parent;
+        Children = new List<TreeNode>();
+    }
+
+    public TreeNode? Parent { get; set; }
+    public string? Value { get; set; }
+
+    public List<TreeNode> Children;
+
+    public void AddChildren(TreeNode node)
+    {
+        Children.Add(node);
+        node.Parent = this;
+    }
+}
